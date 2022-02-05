ee4c1376e6a81524b82061819f478bab2d2eb314
Andrew Bush <apb2471@cs.rit.edu>
2022-02-05 16:40:17 -0500

    format: Add RecieveFromDB file

---
+++ src/main/java/st/symptomtracker/RecieveFromDB.java
@@ -0,0 +1,8 @@
+package st.symptomtracker;
+
+/**
+ * Class containing methods to retrieve symptom data from the
+ * mongoDB. Methods will be called from the DisplaySymptoms class.
+ */
+public class RecieveFromDB {
+}
