b3b124f0a22014040c6919fe2efe2840779715d6
APB2471 <apb2471@cs.rit.edu>
2022-02-05 19:05:42 -0500

    Revert "Development"

--- src/main/java/st/symptomtracker/DisplaySymptoms.java
+++ src/main/java/st/symptomtracker/DisplaySymptoms.java
@@ -1,10 +1,4 @@
 package st.symptomtracker;
 
-/**
- * Call methods from ReceiveFromDB to get
- * get an arraylist of Symptom objects, then
- * construct graphs using JFreeChart.
- */
 public class DisplaySymptoms {
-
 }
